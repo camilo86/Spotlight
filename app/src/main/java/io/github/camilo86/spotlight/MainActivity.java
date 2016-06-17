8681b4593d183070ea6646f1c5deff0d5c9a1c23
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-06-17 17:11:52 -0400

    added gridview basics

--- app/src/main/java/io/github/camilo86/spotlight/MainActivity.java
+++ app/src/main/java/io/github/camilo86/spotlight/MainActivity.java
@@ -2,6 +2,9 @@ package io.github.camilo86.spotlight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(MovieAdapter(this)); // TODO: Create Movie adapter to fill up GridView
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO: look for the movie that was selected, open a detail view and display that movie's info
+            }
+        });
     }
 }
